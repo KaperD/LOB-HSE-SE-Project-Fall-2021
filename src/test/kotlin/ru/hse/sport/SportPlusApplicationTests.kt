1ddd36b05b991e373b0bf6b8474edf1831f1ff5a
Danil Bubnov <dkbubnov@edu.hse.ru>
2021-10-30 13:48:08 +0500

    Init project using Spring initializr

---
+++ src/test/kotlin/ru/hse/sport/SportPlusApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.hse.sport
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SportPlusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
