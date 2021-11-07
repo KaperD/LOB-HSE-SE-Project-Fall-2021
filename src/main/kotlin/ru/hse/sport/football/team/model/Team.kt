3f7af7dfc45ebdda351da9f38ada1f09fc5d189f
Danil Bubnov <dkbubnov@edu.hse.ru>
2021-11-07 12:57:15 +0500

    Change team models

--- src/main/kotlin/ru/hse/sport/football/team/model/Team.kt
+++ src/main/kotlin/ru/hse/sport/football/team/model/Team.kt
@@ -8,35 +8,5 @@ data class Team(
     val homeStadium: String,
     val generalSponsor: String,
     val coachName: String,
-    val playersIds: Array<Int>
-) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Team
-
-        if (id != other.id) return false
-        if (name != other.name) return false
-        if (country != other.country) return false
-        if (league != other.league) return false
-        if (homeStadium != other.homeStadium) return false
-        if (generalSponsor != other.generalSponsor) return false
-        if (coachName != other.coachName) return false
-        if (!playersIds.contentEquals(other.playersIds)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = id
-        result = 31 * result + name.hashCode()
-        result = 31 * result + country.hashCode()
-        result = 31 * result + league.hashCode()
-        result = 31 * result + homeStadium.hashCode()
-        result = 31 * result + generalSponsor.hashCode()
-        result = 31 * result + coachName.hashCode()
-        result = 31 * result + playersIds.contentHashCode()
-        return result
-    }
-}
+    val playersIds: Set<Int>
+)
