248350de7373a3d2f610e75529dd7f0a24a3011c
Ilya Onofriychuk <ilyaon@MacBook-Air-Ilya.local>
2021-11-07 19:32:24 +0300

    Add constraints to TeamDto

--- src/main/kotlin/ru/hse/sport/football/team/model/TeamDto.kt
+++ src/main/kotlin/ru/hse/sport/football/team/model/TeamDto.kt
@@ -1,11 +1,16 @@
 package ru.hse.sport.football.team.model
 
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+
 data class TeamDto(
+    @field:NotBlank(message = "Team name cannot be empty")
     val name: String,
     val country: String,
     val league: String,
     val homeStadium: String,
     val generalSponsor: String,
     val coachName: String,
+    @field:NotNull(message = "It is better to specify an empty set than null")
     val playersIds: Set<Int>
 )
