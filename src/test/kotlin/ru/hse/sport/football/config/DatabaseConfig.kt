446dede974441522d4b6cd9c346f60d9e6f56759
Danil Bubnov <dkbubnov@edu.hse.ru>
2021-11-14 23:14:22 +0500

    Change testcontainers to embedded postgres

--- src/test/kotlin/ru/hse/sport/football/config/DatabaseConfig.kt
+++ src/test/kotlin/ru/hse/sport/football/config/DatabaseConfig.kt
@@ -1,36 +1,24 @@
 package ru.hse.sport.football.config
 
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
+import io.zonky.test.db.postgres.embedded.LiquibasePreparer
+import io.zonky.test.db.postgres.junit5.EmbeddedPostgresExtension
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
-import org.testcontainers.containers.PostgreSQLContainer
 import javax.sql.DataSource
-
-class MyPostgreSQLContainer(imageName: String) : PostgreSQLContainer<MyPostgreSQLContainer>(imageName)
 
 @Configuration
 class DatabaseConfig {
     companion object {
-        val postgreSQLContainer = MyPostgreSQLContainer("postgres:13.4")
-            .withDatabaseName("postgres")
-            .withUsername("postgres")
-            .withPassword("postgres")
-            .apply { start() }
+        var db = EmbeddedPostgresExtension.preparedDatabase(
+            LiquibasePreparer.forClasspathLocation("liquibase/changelog.xml")
+        )
     }
 
     @Bean
     fun dataSource(): DataSource {
-        val config = HikariConfig()
-        config.jdbcUrl = postgreSQLContainer.jdbcUrl
-        config.username = postgreSQLContainer.username
-        config.password = postgreSQLContainer.password
-        config.addDataSourceProperty("cachePrepStmts", "true")
-        config.addDataSourceProperty("prepStmtCacheSize", "250")
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
-
-        return HikariDataSource(config)
+        db.beforeAll(null)
+        return db.testDatabase
     }
 
     @Bean
